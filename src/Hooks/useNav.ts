0c0f2274a1ab73d38046cdd1f48b27bce109ee1a
Johnny <jcarreiropublicidade@gmail.com>
2021-10-24 23:17:10 -0300

    fix: Remove unnecessary console logs

--- src/Hooks/useNav.ts
+++ src/Hooks/useNav.ts
@@ -10,11 +10,6 @@ export const useNav = (navLinkId:string) => {
 
 	const isOnScreen: boolean = useOnScreen(ref)
 
-
-  console.log('IsOnscreen:', isOnScreen, 'NavLinkId:', navLinkId)
-  console.log('Ref:', ref)
-  console.log('Context:',context )
-
   if (isOnScreen) {
     setActiveLink(navLinkId)
   }
